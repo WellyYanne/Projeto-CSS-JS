1ddbccff5bbb59ff1acc3643b7ebe6e037981f1c
WellyYanne <welly.yanne@gmail.com>
2024-03-18 17:00:12 -0300

    inicial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  //pegar o img
+  const img = document.querySelector("#profile img")
+
+//substituir a img
+if(html.classList.contains('light')){
+  img.setAttribute("src", "./assets/Avatar2.png")
+}else{
+ img.setAttribute("src", "./assets/avatarzinho.png")
+}
+
+
+
+
+
+}
